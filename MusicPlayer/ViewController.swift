e39ae3590a98737cc02291741814d928e70884f2
alphachip <fsec1sun@gmail.com>
2020-09-19 20:49:00 +0900

    Initial commit

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -7,14 +7,118 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    // MARK: - Properties
+    var player: AVAudioPlayer!
+    var timer: Timer!
+    
+    // MARK: IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
+    // MARK: - Methods
+    // MARK: Custom Method
+    func initializePlayer() {
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else { // get sound asset using name
+            print("음원 파일 에셋을 가져올 수 없습니다")
+            return
+        }
+        
+        do { // get player
+            // Delegation Pattern : 대리자에 위임하여 일을 대신함
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패")
+            print("코드 : \(error.code), 메세지 : \(error.localizedDescription)")
+        }
+        
+        self.progressSlider.maximumValue = Float(self.player.duration)
+        self.progressSlider.minimumValue = 0
+        self.progressSlider.value = Float(self.player.currentTime)
+    }
+    
+    func updateTimeLabelText(time: TimeInterval) { // TimeInterval : 초
+        let minute: Int = Int(time / 60)
+        // truncatingRemainder : 실수형 나눗셈의 나머지를 반환 (실수형에서 % 연산자 사용 불가)
+        let second: Int = Int(time.truncatingRemainder(dividingBy: 60))
+        let milisecond: Int = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+        
+        let timeText: String = String(format: "%02ld:%02ld:%02ld", minute, second, milisecond)
+        
+        self.timeLabel.text = timeText
+    }
+    
+    func makeAndFireTimer() {
+        // scheduledTimer : withTimeInterval 시간마다 block 호출
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in // unowned self: 약한 연결인 weak와 비슷. 단, nil을 허용하지 않음
+            if self.progressSlider.isTracking { return } // 슬라이더를 움직이는 동안 재생 구간이 바뀌지 않게 함
+            
+            self.updateTimeLabelText(time: self.player.currentTime)
+            self.progressSlider.value = Float(self.player.currentTime)
+        })
+        self.timer.fire()
+    }
+    
+    func invalidateTimer() { // 무효화
+        self.timer.invalidate()
+        self.timer = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initializePlayer()
+    }
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) { // 버튼 클릭 시 동작
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected { // 음악을 재생하고 타이머 재생
+            self.player?.play()
+            self.makeAndFireTimer()
+        } else {
+            self.player?.pause() // 음악을 정지하고 타이머를 무효화
+            self.invalidateTimer()
+        }
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        self.updateTimeLabelText(time: TimeInterval(sender.value))
+        if sender.isTracking { return }
+        self.player.currentTime = TimeInterval(sender.value)
+    }
+    
+        
+    // MARK: AVAudioPlayerDelegate
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        guard let error: Error = error else {
+            print("오디오 플레이어 디코드 오류 발생")
+            return
+        }
+        
+        let message: String
+        message = "오디오 플레이어 오류 발생 \(error.localizedDescription)"
+        
+        let alert: UIAlertController = UIAlertController(title: "알림", message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okAction: UIAlertAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default) { (action: UIAlertAction) -> Void in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.playPauseButton.isSelected = false
+        self.progressSlider.value = 0
+        self.updateTimeLabelText(time: 0)
+        self.invalidateTimer()
+    }
 }
 
